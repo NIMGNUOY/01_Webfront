a49cc1e7316b711757d4f9b874f3290c2ad1b454
NIMGNUOY <ssong9214@gmail.com>
2024-01-29 21:01:20 +0900

    24-01-29

--- FolderForPractice/JS/정규표현식test.js
+++ FolderForPractice/JS/정규표현식test.js
@@ -7,6 +7,7 @@ document.getElementById("inputId").addEventListener("keyup", (e) => {
         
         const inputId = document.getElementById("inputId");
         inputId.innerText = "";
+        inputId.style.background = "white";
         return;
     }
 
@@ -24,6 +25,8 @@ const pw = document.getElementById("pw");
 const checkPw = document.getElementById("checkPw");
 
 checkPw.addEventListener("keyup", (e) => {
+    
+    const pwMessage = document.getElementById("pwMessage");
 
     if (pw.value.length ==0) {
         alert("비밀번호를 입력해주세요");
@@ -32,8 +35,65 @@ checkPw.addEventListener("keyup", (e) => {
         return;
     }
 
+
     if (pw.value == checkPw.value) {
         
+
+        pwMessage.innerHTML = "비밀번호 일치";
+        pwMessage.style.color = "green";
+    } else {
+        
+        pwMessage.innerHTML = "비밀번호 불일치";
+        pwMessage.style.color = "red";
     }
+
+});
+
+// 이름 키를 입력시 문자열 검사
+
+document.getElementById("inputName").addEventListener("keyup", (e) => {
+    
+    const nameMessage = document.getElementById("nameMessage");
+
+    const regExp = /^[ㄱ-힣]{2,5}$/;
+
+    if (regExp.test(e.target.value)) {
+        
+        nameMessage.innerHTML = "정상입력";
+        nameMessage.style.color = "green";
+        
+    } else {
+        
+        nameMessage.innerHTML = "한글만 입력하세요";
+        nameMessage.style.color = "red";
+    }
+
+});
+
+// 성별 체크
+
+const male = document.getElementById("male");
+const female = document.getElementById("female");
+
+if (!male.checked && !female.checked) {
+    alert("성별을 선택해주세요.");
+}
+
+// 전화번호 형식 검사
+
+document.getElementById("inputCelNum").addEventListener("keyup", (e) => {
+    
+    const regExp = /^[0][0-9]{1,3}\-[0-9]{4}\-[0-9]{4}/;
+
+    const celMessage = document.getElementById("celMessage");
+
+    if(regExp.test(e.target.value)) {
+
+        celMessage.innerHTML = "확인";
+        e.target.style.backgroundColor = "springgreen";
+        
+    } else {
+        alert("전화번호의 형식이 올바르지 않습니다.")
+    };
 
 });
